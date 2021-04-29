eb32e2ab5dd4c9fc8d22ae90340db42b76edef45
mantzinos <mantzinos@hotmail.com>
2021-04-29 22:40:08 +0300

    again

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Landing = () => {
+  console.log(process.env.SERVER_URL);
   return (
     <section className="landing">
       <div className="land">
