f8fba66f638851fd3d682f0d1ad8bb504779ce15
mantzinos <mantzinos@hotmail.com>
2021-04-29 23:09:24 +0300

    again ... A

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
+require("dotenv").config();
 
 const Landing = () => {
   const aa = process.env.SERVER_URL;
-  console.log("dose gamo leme", aa);
+  console.log("dose gamo leme aaa ", aa);
   // console.log(process.env.SERVER_URL);
   return (
     <section className="landing">
